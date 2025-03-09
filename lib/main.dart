db136b447b52732b28c456bfc0ea5f97f16e3ea9
moshiurr4766 <rahman15-4766@diu.edu.bd>
2025-03-09 11:15:13 +0600

    Test_1

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
 
+import 'package:ecommerce_app/screens/home/pages/home.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(const MyEcom());
 }
 
+class MyEcom extends StatelessWidget {
+  const MyEcom({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Freevies E-Commerce',
+      home: Home(),
+    );
+  }
+}
